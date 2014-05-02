af0c78d328da14cd7348b399e4ebc46ee15e7718
igor-kondratiev <igor.kondratiev93@gmail.com>
2014-05-02 06:28:45 +0300

    fixed lsa method

--- bookshelf/books/management/commands/make_lsa.py
+++ bookshelf/books/management/commands/make_lsa.py
@@ -1,4 +1,5 @@
-from numpy import zeros, sum, asarray
+from math import isinf
+from numpy import zeros, sum, dot, diag
 from numpy.linalg import svd
 from numpy.ma import log
 
@@ -24,35 +25,43 @@ class Command(BaseCommand):
 
         super(Command, self).__init__()
 
-    def perform_build(self):
-        self.keys = [k for k in self.words_dict.keys() if len(self.words_dict[k]) > 1]
-        self.keys.sort()
-
-        self.a = zeros([len(self.keys), self.books_count])
-        for i, k in enumerate(self.keys):
-            for d in self.words_dict[k]:
-                self.a[i, d] += 1
-
-        print 'Valuable words found - {0}'.format(len(self.keys))
-
-    # todo: fix it
     def perform_tf_idf(self):
         words_per_doc = sum(self.a, axis=0)
-        docs_per_word = sum(asarray(self.a > 0, 'i'), axis=1)
+        docs_per_word = sum(self.a, axis=1)
         rows, cols = self.a.shape
         for i in range(rows):
             for j in range(cols):
                 self.a[i, j] = (self.a[i, j] / words_per_doc[j]) * log(float(cols) / docs_per_word[i])
+                if isinf(self.a[i, j]):
+                    print "Infinity found!"
 
     def perform_svd(self):
         self.u, self.s, self.vt = svd(self.a, full_matrices=False)
+
+        print "SVD done."
+
+        s_count = self.s.shape[0]
+        valuable_count = (s_count + 1) / 2
+        for i in range(valuable_count, s_count):
+            self.s[i] = 0
+
+        self.a = dot(dot(self.u, diag(self.s)), self.vt)
+
+        print "Rank reduced."
 
     def handle(self, *args, **options):
         books = Book.objects.all()[:self.BOOKS_TO_PROCESS]
         self.books_count = len(books)
 
-        for i, book in enumerate(books):
+        readers = []
+        for book in books:
             reader = BookReader(book)
+            if reader.words_count == 0:
+                continue
+
+            readers.append(reader)
+            i = len(readers) - 1
+
             for word in reader.words_list:
                 if word in self.words_dict:
                     self.words_dict[word].append(i)
@@ -63,24 +72,32 @@ class Command(BaseCommand):
 
         print 'total words count = {0}'.format(len(self.words_dict))
 
-        self.perform_build()
-        # self.perform_tf_idf()
+        self.books_count = len(readers)
+
+        self.keys = [k for k in self.words_dict.keys()]
+        self.keys.sort()
+
+        self.a = zeros([len(self.keys), self.books_count])
+        for i, k in enumerate(self.keys):
+            for d in self.words_dict[k]:
+                self.a[i, d] = readers[d].words[k]
+
+        print 'Valuable words found - {0}'.format(len(self.keys))
+
+        self.perform_tf_idf()
         self.perform_svd()
 
         print 'svd done. len(s) = {0}'.format(self.s.shape)
 
-        w = self.s.shape[0]
-        valuable = w / 2
-        res_u = self.u[:, :valuable]
         for i in range(self.books_count):
             for j in range(i + 1, self.books_count):
                 distance = 0.0
-                for k in range(valuable):
-                    distance += (res_u[i, k] - res_u[j, k]) * (res_u[i, k] - res_u[j, k])
+                for k in range(len(self.keys)):
+                    distance += (self.a[k, i] - self.a[k, j]) * (self.a[k, i] - self.a[k, j])
                 distance = pow(distance, 0.5)
                 db_distance = BookDistance(
-                    first_book=books[i],
-                    second_book=books[j],
+                    first_book=readers[i].db_book,
+                    second_book=readers[j].db_book,
                     distance=distance
                 )
                 db_distance.save()
