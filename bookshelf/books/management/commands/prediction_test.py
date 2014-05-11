f3bc77ea38ffe451b5f50c092e32b8161bf45405
igor-kondratiev <igor.kondratiev93@gmail.com>
2014-05-11 16:29:50 +0300

    finished predictors test

--- bookshelf/books/management/commands/prediction_test.py
+++ bookshelf/books/management/commands/prediction_test.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import importlib
 import operator
+from django.db.models import Count
 import numpy as np
 
 from math import sqrt
@@ -19,7 +20,12 @@ class Command(BaseCommand):
 
     # Список классов предикторов, которые нужно тестировать
     PREDICTORS = [
-        'books.predictors.AverageBookMarkPredictor'
+        'books.predictors.ItemToItemCollaborativeCosinePredictor',
+        'books.predictors.ItemToItemCollaborativePiersonPredictor',
+        'books.predictors.ItemToItemLSAPredictor',
+        'books.predictors.AverageBookMarkPredictor',
+        'books.predictors.UserToUserCollaborativeCosinePredictor',
+        'books.predictors.UserToUserCollaborativePiersonPredictor',
     ]
 
     @staticmethod
@@ -39,9 +45,10 @@ class Command(BaseCommand):
 
         # Словарь отклонений для каждого предиктора
         self._rmse = {}
+        self._predicted_count = {}
 
         # Загружаем книги и пользователей
-        self._users = list(User.objects.all())
+        self._users = list(User.objects.all().annotate(marks_count=Count('bookmark')).filter(marks_count__gte=7))
         self._users_dict = dict((user, index) for index, user in enumerate(self._users))
 
         self._books = list(Book.objects.all())
@@ -53,9 +60,14 @@ class Command(BaseCommand):
         self._source_marks = []
         self._test_marks = []
         for user in self._users:
-            # У каждого пользователя забираем 1/3 оценок в тестовую выборку
+            # У каждого пользователя, у которого не менее 8 оценок,
+            # забираем 2 оценки в тестовую выборку
             marks_count = user.bookmark_set.count()
-            limit = marks_count - marks_count / 3
+            if marks_count < 12:
+                self._source_marks.extend(list(user.bookmark_set.all()))
+                continue
+
+            limit = marks_count - marks_count / 4
             self._source_marks.extend(list(user.bookmark_set.all()[:limit]))
             self._test_marks.extend(list(user.bookmark_set.all()[limit:]))
 
@@ -64,9 +76,9 @@ class Command(BaseCommand):
         print 'Загружено {0} книг, {1} пользователей, {2} оценок'.format(
             len(self._books),
             len(self._users),
-            len(self._all_marks)
+            len(self._source_marks) + len(self._test_marks)
         )
-        print '{0} оценок попало в исходную выборку, {1} оценок в тестовую'.format(
+        print '{0} оценок попало в исходную выборку, {1} оценок - в тестовую'.format(
             len(self._source_marks),
             len(self._test_marks)
         )
@@ -79,25 +91,38 @@ class Command(BaseCommand):
             book_index = self._books_dict[mark.book]
             prediction_matrix[user_index, book_index] = mark.mark
 
-        predictors = [predictor(prediction_matrix, self._users_dict, self._books_dict) for predictor in self._predictor_classes]
+        print 'Загрузка пердикторов...'
+        predictors = []
+        for i, predictor in enumerate(self._predictor_classes):
+            predictors.append(predictor(prediction_matrix, self._users_dict, self._books_dict))
+            print '{0} из {1} загружено.'.format(i+1, len(self._predictor_classes))
+
         print 'Загружено предикторов - {0}:'.format(len(predictors))
         for predictor in predictors:
             print predictor.__class__.__name__
 
         for predictor in predictors:
             self._rmse[predictor] = 0
+            self._predicted_count[predictor] = 0
 
         for mark in self._test_marks:
             for predictor in predictors:
                 predicted_mark = predictor.predict(mark.user, mark.book)
-                self._rmse[predictor] += (mark.mark - predicted_mark) * (mark.mark - predicted_mark)
+                if predicted_mark > 0:
+                    self._rmse[predictor] += (mark.mark - predicted_mark) * (mark.mark - predicted_mark)
+                    self._predicted_count[predictor] += 1
 
         for predictor in predictors:
-            self._rmse[predictor] = sqrt(self._rmse[predictor] / len(self._test_marks))
+            if self._predicted_count[predictor]:
+                self._rmse[predictor] = sqrt(self._rmse[predictor] / self._predicted_count[predictor])
 
         ordered_rmse = sorted(self._rmse.iteritems(), key=operator.itemgetter(1))
         print 'Результаты тестирования - RMSE для каждого предиктора:'
         for rmse in ordered_rmse:
-            print '{0} - {1}'.format(rmse[0].__class__.__name__, rmse[1])
+            print '{0} - спрогнозировано {1} оценок. RMSE = {2}.'.format(
+                rmse[0].__class__.__name__,
+                self._predicted_count[rmse[0]],
+                rmse[1]
+            )
 
         print 'Тестирование завершено.'
--- bookshelf/books/predictors/__init__.py
+++ bookshelf/books/predictors/__init__.py
@@ -1,4 +1,11 @@
 # coding=utf-8
+import numpy as np
+
+from math import sqrt
+
+from books.models import BookDistance
+
+
 class BasePredictor(object):
     """
     Базовый класс для всех классов-предикторов
@@ -57,3 +64,318 @@ class AverageBookMarkPredictor(BasePredictor):
         Просто возвращаем предпросчитанную среднюю оценку
         """
         return self._book_marks[self._books_dict[book]]
+
+
+class UserToUserCollaborativeCosinePredictor(BasePredictor):
+    """
+    Реализует алгоритм колаборативной фильтрации на основе
+    схожести пользователей, использует косинусную метрику
+    """
+
+    # Минимальныцй уровень похожести для учета при прогнозировании
+    MINIMUM_SIMILARITY = 0.0
+
+    def __init__(self, matrix, users_dict, books_dict):
+        """
+        Конструктор, сразу рассчитывает схожесть пользователей
+        """
+        super(UserToUserCollaborativeCosinePredictor, self).__init__(matrix, users_dict, books_dict)
+
+        self._similarities_matrix = np.zeros([len(self._users_dict), len(self._users_dict)])
+
+        for i in range(len(self._users_dict)):
+            for j in range(i + 1, len(self._users_dict)):
+                x = 0.0
+                y = 0.0
+                x_dot_y = 0.0
+
+                for k in range(len(self._books_dict)):
+                    if self._matrix[i, k] > 0 and self._matrix[j, k] > 0:
+                        x += self._matrix[i, k] * self._matrix[i, k]
+                        y += self._matrix[j, k] * self._matrix[j, k]
+                        x_dot_y += self._matrix[i, k] * self._matrix[j, k]
+
+                similarity = 0.0
+                if x > 0 and y > 0:
+                    x = sqrt(x)
+                    y = sqrt(y)
+                    similarity = x_dot_y / (x * y)
+
+                self._similarities_matrix[i, j] = similarity
+                self._similarities_matrix[j, i] = similarity
+
+    def predict(self, user, book):
+        """
+        Просто считаем средневзвешенную оценку на основании
+        матрицы похожетстей пользователей
+        """
+        user_index = self._users_dict[user]
+        book_index = self._books_dict[book]
+
+        predicted_mark = 0.0
+        total_weights = 0.0
+        for u in range(len(self._users_dict)):
+            if self._matrix[u, book_index] > 0 and self._similarities_matrix[user_index, u] > self.MINIMUM_SIMILARITY:
+                predicted_mark += self._matrix[u, book_index] * self._similarities_matrix[user_index, u]
+                total_weights += self._similarities_matrix[user_index, u]
+
+        if total_weights > 0:
+            predicted_mark /= total_weights
+
+        return predicted_mark
+
+
+class UserToUserCollaborativePiersonPredictor(BasePredictor):
+    """
+    Реализует алгоритм колаборативной фильтрации на основе
+    схожести пользователей, использует корреляцию Пирсона
+    """
+
+    # Минимальныцй уровень похожести для учета при прогнозировании
+    MINIMUM_SIMILARITY = 0.0
+
+    def __init__(self, matrix, users_dict, books_dict):
+        """
+        Конструктор, сразу рассчитывает схожесть пользователей
+        """
+        super(UserToUserCollaborativePiersonPredictor, self).__init__(matrix, users_dict, books_dict)
+
+        self._similarities_matrix = np.zeros([len(self._users_dict), len(self._users_dict)])
+        self._average_marks = np.zeros([len(self._users_dict)])
+
+        for u in range(len(self._users_dict)):
+            count = 0
+            for b in range(len(self._books_dict)):
+                if self._matrix[u, b] > 0:
+                    self._average_marks[u] += matrix[u, b]
+                    count += 1
+            self._average_marks[u] /= count
+
+        for i in range(len(self._users_dict)):
+            for j in range(i + 1, len(self._users_dict)):
+                x = 0.0
+                y = 0.0
+                x_dot_y = 0.0
+
+                for k in range(len(self._books_dict)):
+                    if self._matrix[i, k] > 0 and self._matrix[j, k] > 0:
+                        x += (self._matrix[i, k] - self._average_marks[i]) * (self._matrix[i, k] - self._average_marks[i])
+                        y += (self._matrix[j, k] - self._average_marks[j]) * (self._matrix[j, k] - self._average_marks[j])
+                        x_dot_y += (self._matrix[i, k] - self._average_marks[i]) * (self._matrix[j, k] - self._average_marks[j])
+
+                similarity = 0.0
+                if x > 0 and y > 0:
+                    x = sqrt(x)
+                    y = sqrt(y)
+                    similarity = x_dot_y / (x * y)
+
+                self._similarities_matrix[i, j] = similarity
+                self._similarities_matrix[j, i] = similarity
+
+    def predict(self, user, book):
+        """
+        Просто считаем средневзвешенную оценку на основании
+        матрицы похожетстей пользователей
+        """
+        user_index = self._users_dict[user]
+        book_index = self._books_dict[book]
+
+        predicted_mark = 0.0
+        total_weights = 0.0
+        for u in range(len(self._users_dict)):
+            if self._matrix[u, book_index] > 0 and self._similarities_matrix[user_index, u] > self.MINIMUM_SIMILARITY:
+                predicted_mark += (self._matrix[u, book_index] - self._average_marks[u]) * self._similarities_matrix[user_index, u]
+                total_weights += self._similarities_matrix[user_index, u]
+
+        if total_weights > 0:
+            predicted_mark /= total_weights
+
+        predicted_mark += self._average_marks[user_index]
+
+        return predicted_mark
+
+
+class ItemToItemLSAPredictor(BasePredictor):
+    """
+    Прогнозирует оценку как средневзвешенную оценку других
+    книг данного пользователя с учетом корреляций, посчитанных по LSA
+    """
+
+    # Минимальныцй уровень похожести для учета при прогнозировании
+    MINIMUM_SIMILARITY = 0.4
+
+    def __init__(self, matrix, users_dict, books_dict):
+        """
+        Конструктор, сразу инициализирует матрицу корреляций
+        """
+        super(ItemToItemLSAPredictor, self).__init__(matrix, users_dict, books_dict)
+
+        self._similarities_matrix = np.zeros([len(self._books_dict), len(self._books_dict)])
+
+        distances = list(BookDistance.objects.all())
+        for distance in distances:
+            self._similarities_matrix[self._books_dict[distance.first_book], self._books_dict[distance.second_book]] = distance.distance
+            self._similarities_matrix[self._books_dict[distance.second_book], self._books_dict[distance.first_book]] = distance.distance
+
+    def predict(self, user, book):
+        """
+        Просто считаем средневзвешенную оценку на основании
+        матрицы похожетстей книг
+        """
+        user_index = self._users_dict[user]
+        book_index = self._books_dict[book]
+
+        predicted_mark = 0.0
+        total_weights = 0.0
+        for b in range(len(self._books_dict)):
+            if self._matrix[user_index, b] > 0 and self._similarities_matrix[book_index, b] > self.MINIMUM_SIMILARITY:
+                predicted_mark += self._matrix[user_index, b] * self._similarities_matrix[book_index, b]
+                total_weights += self._similarities_matrix[book_index, b]
+
+        if total_weights > 0:
+            predicted_mark /= total_weights
+
+        return predicted_mark
+
+
+class ItemToItemCollaborativeCosinePredictor(BasePredictor):
+    """
+    Реализует алгоритм колаборативной фильтрации на основе
+    схожести книг, использует косинусную метрику
+    """
+
+    # Минимальныцй уровень похожести для учета при прогнозировании
+    MINIMUM_SIMILARITY = 0.0
+
+    def __init__(self, matrix, users_dict, books_dict):
+        """
+        Конструктор, сразу рассчитывает схожесть книг
+        """
+        super(ItemToItemCollaborativeCosinePredictor, self).__init__(matrix, users_dict, books_dict)
+
+        self._similarities_matrix = np.zeros([len(self._books_dict), len(self._books_dict)])
+
+        for i in range(len(self._books_dict)):
+            for j in range(i + 1, len(self._books_dict)):
+                x = 0.0
+                y = 0.0
+                x_dot_y = 0.0
+
+                for k in range(len(self._users_dict)):
+                    if self._matrix[k, i] > 0 and self._matrix[k, j] > 0:
+                        x += self._matrix[k, i] * self._matrix[k, i]
+                        y += self._matrix[k, j] * self._matrix[k, j]
+                        x_dot_y += self._matrix[k, i] * self._matrix[k, j]
+
+                similarity = 0.0
+                if x > 0 and y > 0:
+                    x = sqrt(x)
+                    y = sqrt(y)
+                    similarity = x_dot_y / (x * y)
+
+                self._similarities_matrix[i, j] = similarity
+                self._similarities_matrix[j, i] = similarity
+
+    def predict(self, user, book):
+        """
+        Просто считаем средневзвешенную оценку на основании
+        матрицы похожетстей книг
+        """
+        user_index = self._users_dict[user]
+        book_index = self._books_dict[book]
+
+        predicted_mark = 0.0
+        total_weights = 0.0
+        for b in range(len(self._books_dict)):
+            if self._matrix[user_index, b] > 0 and self._similarities_matrix[book_index, b] > self.MINIMUM_SIMILARITY:
+                predicted_mark += self._matrix[user_index, b] * self._similarities_matrix[book_index, b]
+                total_weights += self._similarities_matrix[book_index, b]
+
+        if total_weights > 0:
+            predicted_mark /= total_weights
+
+        return predicted_mark
+
+
+class ItemToItemCollaborativePiersonPredictor(BasePredictor):
+    """
+    Реализует алгоритм колаборативной фильтрации на основе
+    схожести книг, использует корреляцию Пирсона
+    """
+
+    # Минимальныцй уровень похожести для учета при прогнозировании
+    MINIMUM_SIMILARITY = 0.0
+
+    def __init__(self, matrix, users_dict, books_dict):
+        """
+        Конструктор, сразу рассчитывает схожесть книг
+        """
+        super(ItemToItemCollaborativePiersonPredictor, self).__init__(matrix, users_dict, books_dict)
+
+        self._similarities_matrix = np.zeros([len(self._books_dict), len(self._books_dict)])
+        self._average_marks = np.zeros([len(self._books_dict)])
+
+        for b in range(len(self._books_dict)):
+            count = 0
+            for u in range(len(self._users_dict)):
+                if self._matrix[u, b] > 0:
+                    self._average_marks[b] += float(matrix[u, b])
+                    count += 1
+            self._average_marks[b] /= count
+
+        for i in range(len(self._books_dict)):
+            for j in range(i + 1, len(self._books_dict)):
+                x = 0.0
+                y = 0.0
+                x_dot_y = 0.0
+
+                for k in range(len(self._users_dict)):
+                    if self._matrix[k, i] > 0 and self._matrix[k, j] > 0:
+                        x += (self._matrix[k, i] - self._average_marks[i]) * (self._matrix[k, i] - self._average_marks[i])
+                        y += (self._matrix[k, j] - self._average_marks[j]) * (self._matrix[k, j] - self._average_marks[j])
+                        x_dot_y += (self._matrix[k, i] - self._average_marks[i]) * (self._matrix[k, j] - self._average_marks[j])
+
+                similarity = 0.0
+                if x > 0 and y > 0:
+                    x = sqrt(x)
+                    y = sqrt(y)
+                    similarity = x_dot_y / (x * y)
+
+                self._similarities_matrix[i, j] = similarity
+                self._similarities_matrix[j, i] = similarity
+
+    def predict(self, user, book):
+        """
+        Просто считаем средневзвешенную оценку на основании
+        матрицы похожестей книг
+        """
+        user_index = self._users_dict[user]
+        book_index = self._books_dict[book]
+
+        predicted_mark = 0.0
+        total_weights = 0.0
+        for b in range(len(self._books_dict)):
+            if self._matrix[user_index, b] > 0 and self._similarities_matrix[book_index, b] > self.MINIMUM_SIMILARITY:
+                predicted_mark += (self._matrix[user_index, b] - self._average_marks[b]) * self._similarities_matrix[book_index, b]
+                total_weights += self._similarities_matrix[book_index, b]
+
+        if total_weights > 0:
+            predicted_mark /= total_weights
+
+        predicted_mark += self._average_marks[book_index]
+
+        return predicted_mark
+
+
+class HybridPredictor(BasePredictor):
+    def __init__(self, matrix, users_dict, books_dict):
+        super(HybridPredictor, self).__init__(matrix, users_dict, books_dict)
+        self._utu_predictor = UserToUserCollaborativePiersonPredictor(matrix, users_dict, books_dict)
+        self._lsa_predictor = ItemToItemLSAPredictor(matrix, users_dict, books_dict)
+
+    def predict(self, user, book):
+        utu_mark = self._utu_predictor.predict(user, book)
+        lsa_mark = self._lsa_predictor.predict(user, book)
+        if utu_mark > 0 and lsa_mark > 0:
+            return 0.5 * utu_mark + 0.5 * lsa_mark
+        return max(utu_mark, lsa_mark)
