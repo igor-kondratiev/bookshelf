8d0b0a039a754ce8f16f8199dfc99137eb130042
igor-kondratiev <igor.kondratiev93@gmail.com>
2014-05-18 13:25:12 +0300

    Some fixes

--- bookshelf/books/views.py
+++ bookshelf/books/views.py
@@ -11,7 +11,7 @@ from books.models import Book
 
 
 def home_view(request):
-    popular_books = Book.objects.annotate(mark=Avg('bookmark')).order_by('-mark')[:12]
+    popular_books = Book.objects.annotate(mark=Avg('bookmark__mark')).exclude(mark=None).order_by('-mark')[:12]
     readable_books = Book.objects.annotate(marks_count=Count('bookmark')).order_by('-marks_count')[:6]
     return render(request, 'index.html', {
         'popular_books': popular_books,
